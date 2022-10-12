85e9e8b6a164568dc76e36be74f2c91171363f9b
“Reshma <3areshma@email.com>
2022-10-12 13:44:13 -0500

    note App project 3

---
+++ src/main/java/com/devmountain/noteApp/controllers/NoteController.java
@@ -0,0 +1,5 @@
+package com.devmountain.noteApp.controllers;
+
+public class NoteController {
+
+}
---
+++ src/main/java/com/devmountain/noteApp/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.devmountain.noteApp.controllers;
+
+import com.devmountain.noteApp.dtos.UserDto;
+import com.devmountain.noteApp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public List<String> addUser(@RequestMapping UserDto userDto){
+        String passHash = passwordEncoder.encode(userDto.getPassword());
+        userDto.setPassword(passHash);
+        return  userService.addUser((userDto));
+    }
+}
